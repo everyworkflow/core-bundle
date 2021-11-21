93d4eab0d189af1940b8ffa3e19bddb7678b6477
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:07:58 +0000

    Added ?ExtensionInterface

--- EveryWorkflowCoreBundle.php
+++ EveryWorkflowCoreBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\CoreBundle;
 
 use EveryWorkflow\CoreBundle\DependencyInjection\CoreExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowCoreBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new CoreExtension();
     }
